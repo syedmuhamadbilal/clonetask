95727e26e5f9f2af143edcd510d6b5e4d456af2e
Irtisam24 <irtisam.d@gmail.com>
2021-07-17 20:36:50 +0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ImportFormPage from "./pages/ImportFormPage";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route>
+          (
+          <>
+            <div>
+              <p>Bilal page will be here</p>
+            </div>
+          </>
+          )
+        </Route>
+        <Route to="/import">
+          <ImportFormPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
